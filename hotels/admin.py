c9357c4068947d44c43c0fd6983af33bdd5914fb
AyrexSigma <ayrex.ytz@gmail.com>
2025-06-28 16:36:38 +0300

    git create admin.py

---
+++ hotels/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Hotel, Room, Booking
+
+# Register your models here.
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'stars', 'has_pool')
+    list_filter = ('stars', 'has_pool', 'has_spa')
+    search_fields = ('name', 'address')
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('hotel', 'room_type', 'room_number', 'price_per_night', 'is_available')
+    list_filter = ('room_type', 'is_available')
+    search_fields = ('room_number', 'hotel__name')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'check_in', 'check_out', 'is_confirmed')
+    list_filter = ('is_confirmed', 'check_in', 'check_out')
+    search_fields = ('user__username', 'room__room_number')
